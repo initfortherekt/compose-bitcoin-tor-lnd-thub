9059abd9aad24b7f5176f7087a84a8b2f1c1ce5b
richdbtc <>
2021-06-30 21:50:38 -0500

    Finished bitcoind

--- bitcoind/bitcoin-entrypoint.sh
+++ bitcoind/bitcoin-entrypoint.sh
@@ -8,16 +8,16 @@ chown -R bitcoin:bitcoin /data
 ln -sfn /data /home/bitcoin/.bitcoin 
 chown -h bitcoin:bitcoin /home/bitcoin/.bitcoin
 
-if [[ "$BITCOIN_WALLETDIR" ]] && [[ "$BITCOIN_NETWORK" ]]; then
-	WALLETDIR="$BITCOIN_WALLETDIR/${BITCOIN_NETWORK}"
-	WALLETFILE="${WALLETDIR}/wallet.dat"
-	mkdir -p "$WALLETDIR"	
-	chown -R bitcoin:bitcoin "$WALLETDIR"
-	if ! [[ -f "${WALLETFILE}" ]]; then
-	  echo "The wallet does not exists, creating it at ${WALLETDIR}..."
-	  gosu bitcoin bitcoin-wallet "-datadir=${WALLETDIR}" "-wallet=" create
-	fi
-fi
+# if [[ "$BITCOIN_WALLETDIR" ]] && [[ "$BITCOIN_NETWORK" ]]; then
+# 	WALLETDIR="$BITCOIN_WALLETDIR/${BITCOIN_NETWORK}"
+# 	WALLETFILE="${WALLETDIR}/wallet.dat"
+# 	mkdir -p "$WALLETDIR"	
+# 	chown -R bitcoin:bitcoin "$WALLETDIR"
+# 	if ! [[ -f "${WALLETFILE}" ]]; then
+# 	  echo "The wallet does not exists, creating it at ${WALLETDIR}..."
+# 	  gosu bitcoin bitcoin-wallet "-datadir=${WALLETDIR}" "-wallet=" create
+# 	fi
+# fi
 
 exec gosu bitcoin "$@"
 
